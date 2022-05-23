d1a7129578cf1defe37297796b338bcada196add
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-05-23 10:34:52 -0300

    first commit

---
+++ model/questao.ts
@@ -0,0 +1,39 @@
+import RespostaModel from "./resposta";
+
+export default class QuestaoModel {
+    #id: number
+    #enunciado: string
+    #respostas: RespostaModel[]
+    #acertou: boolean
+
+    constructor(id: number, enunciado: string, respostas: any[], acertou: boolean) {
+        this.#id = id
+        this.#enunciado = enunciado
+        this.#respostas = respostas
+        this.#acertou = acertou
+    }
+
+    get id() {
+        return this.#id
+    }
+
+    get enunciado() {
+        return this.#enunciado
+    }
+
+    get respostas() {
+        return this.#id
+    }
+
+    get acertou() {
+        return this.#id
+    }
+    
+    get respondida() {
+        for (let resposta of this.#respostas) {
+            if (resposta.revelada) return true
+        }
+
+        return false
+    }
+}
